7e4f5b593a0a0382147e7d3ae2baf039c83ee2fa
Khan Md. Hasib <32868239+hasibaust13@users.noreply.github.com>
2020-12-15 00:12:53 +0600

    Add files via upload

---
+++ Java/Day01WelcomeToJava.java
@@ -0,0 +1,16 @@
+
+package day.pkg01.welcome.to.java;
+
+/**
+ *
+ * @author Khan Md. Hasib
+ */
+public class Day01WelcomeToJava {
+
+    
+    public static void main(String[] args) {
+         System.out.println("Hello, World.");
+         System.out.println("Hello, Java.");
+    }
+    
+}
